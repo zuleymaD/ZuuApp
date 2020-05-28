bc4e8f77928ad73b63cf969cf811c150d55bb652
Zuleyma <zvazquez@iwa.com.mx>
2020-05-27 22:23:43 -0500

    version1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Song } from './model/song';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  songList: Song[] = [
+     {id: 1, name:" 2 Locos", gender:"reggaeton"},
+     {id: 2, name:"Rompecorazones", gender:"reggaeton"}
+  ];
+  selectedSong: any;
+  form: FormGroup;
+
+
+constructor(
+  private fb: FormBuilder,
+){
+  console.log(this.songList)
+  this.form = fb.group({
+    id:[""],
+    name:[""],
+    gender:[""]
+  })
+}
+
+openForEdit(song:Song){
+  this.form.get("id").setValue(song.id);
+  this.form.get("name").setValue(song.name);
+  this.form.get("gender").setValue(song.gender);
+}
+
+addOrEdit(){
+  if(this.form.value.id === ""){
+  
+    this.selectedSong = {
+      id : this.songList.length +1,
+      name: this.form.value.name,
+      gender: this.form.value.gender
+    }
+      this.songList.push(this.selectedSong);
+  }else{
+    this.songList.map(element => {
+      if(element.id === this.form.value.id ){
+        element.name = this.form.value.name;
+        element.gender = this.form.value.gender
+      }
+      return element;
+    })
+  }
+  this.form.get("id").setValue("");
+  this.form.get("name").setValue("");
+  this.form.get("gender").setValue("");
+}
+
+delete(id){
+  let index
+  const x=  this.songList.filter((elemente , i)=> {
+    if(elemente.id === id){
+      index =i
+    }} )
+
+    this.songList.splice(index,1)
+
+    this.form.get("id").setValue("");
+    this.form.get("name").setValue("");
+    this.form.get("gender").setValue("");
+}
 }
---
+++ src/app/model/song.ts
@@ -0,0 +1,5 @@
+export class Song{
+    id: number = 0;
+    name: string;
+    gender: string;
+}
